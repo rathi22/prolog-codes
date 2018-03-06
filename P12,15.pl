266be13a40d5614a88fa183a0f6204456b17114b
rathi22 <30855764+rathi22@users.noreply.github.com>
2018-03-06 21:36:27 +0530

    Update and rename P12.pl to P12,15.pl

--- P12,15.pl
+++ P12,15.pl
@@ -7,3 +7,9 @@ uncompress([[X,Y]|L],L1):-
     append(L3,L2,L1),!.
 uncompress([X|L],[X|L1]):-
     uncompress(L,L1).
+
+rep(0,_,[]).
+rep(N,X,[X|L]):- N1 is N-1, rep(N1,X,L),!.
+dupli([],_,[]).
+dupli([X|L1],N,L2):-
+    dupli(L1,N,L3), rep(N,X,Y), append(Y,L3,L2),!.
