cef318054195d68d4a9b278d2679a7f5af306eae
rathi22 <30855764+rathi22@users.noreply.github.com>
2018-03-08 00:02:11 +0530

    Update coin-change.pl

--- coin-change.pl
+++ coin-change.pl
@@ -3,3 +3,9 @@ cc(Amt,[_|L],[0|Ans]):-
     cc(Amt,L,Ans).
 cc(Amt,[X|L],[Y|L1]):-
     Amt >= X, Amt1 is Amt-X, cc(Amt1,[X|L],[Y1|L1]), Y is Y1+1.
+
+nways(0,_,1).
+nways(Amt,_,0):- Amt<0,!.
+nways(Amt,[],0):- Amt>0,!.
+nways(Amt,[X|L],Ans):-
+    Amt>0, nways(Amt,L,Ans1), Amt1 is Amt-X, nways(Amt1,[X|L],Ans2), Ans is Ans1+Ans2.
